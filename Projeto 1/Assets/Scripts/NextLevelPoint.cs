049ef056e15b2a38407285db3a11543b4d474304
DyogoDG <DyogoDG@hotmail.com>
2024-05-14 06:37:24 -0300

    As 2 primeiras fases foram refeitas e temos a scena Start Game OK

--- Projeto 1/Assets/Scripts/NextLevelPoint.cs
+++ Projeto 1/Assets/Scripts/NextLevelPoint.cs
@@ -6,6 +6,12 @@ using UnityEngine.SceneManagement;
 public class NextLevelPoint : MonoBehaviour
 {
     public string lvlName;
+
+    public void LoadScene1()
+    {
+        SceneManager.LoadScene("lvl_1");
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
